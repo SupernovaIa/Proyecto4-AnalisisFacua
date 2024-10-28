84c066b2ea85e8992fb5ddf8abb9be9bf28dce6b
jcarreir <jav.racec@gmail.com>
2024-10-28 01:12:13 +0100

    EDA Done

--- src/support.py
+++ src/support.py
@@ -347,3 +347,60 @@ def data_insertion(query: str, values: list):
         if connection:
             connection.close()
             print("Database connection closed.")
+
+
+def sql_query(query: str):
+    """
+    Executes a given SQL query on a PostgreSQL database and retrieves all results.
+
+    Parameters:
+    - query (str): The SQL query to be executed on the database.
+
+    Returns:
+    - result (list of tuples | None): A list of tuples containing the query results if the execution is successful, or `None` if an error occurs.
+    """
+
+    # Initialize result to avoid returning undefined variable
+    result = None
+    connection = None
+    
+    try:
+        # Establish the connection
+        connection = psycopg2.connect(
+            database='supermarkets',
+            user='my_user',
+            password='admin',
+            host='localhost',
+            port='5432'
+        )
+
+        # Query execution
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchall()
+        
+        print("Query performed successfully.")
+
+    except OperationalError as e:
+        if e.pgcode == errorcodes.INVALID_PASSWORD:
+            print('Incorrect password')
+        elif e.pgcode == errorcodes.CONNECTION_EXCEPTION:
+            print('Connection error')
+        elif e.pgcode == errorcodes.INVALID_CATALOG_NAME:
+            print('Database does not exist')
+        else:
+            print(f"Database error: {e}")
+            
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+    finally:
+        # Closing connection
+        if connection:
+            try:
+                connection.close()
+                print("Database connection closed.")
+            except Exception as e:
+                print(f"Error closing connection: {e}")
+
+    return result
